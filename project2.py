6bb7330b485229ea9f3d1c1f35f46cf599f66851
kevinjohn2410 <160509097+kevinjohn2410@users.noreply.github.com>
2024-02-26 20:16:43 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,10 @@
+#Traffic light simulator
+colour=str(input("input any colour : "))
+if (colour=="red"):
+   print("stop")
+elif (colour=="yellow"):
+    print("slow down & wait")
+elif (colour=="green"):
+    print("go")
+else:
+    print("add correct colour")
